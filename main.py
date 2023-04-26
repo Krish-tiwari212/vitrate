324a5e746b19d2a9467e44f3107617c95a6a8778
Krish-tiwari212 <krish.tiwari2022@vitstudent.ac.in>
2023-04-26 21:30:50 +0530

    Initial Commit

--- main.py
+++ main.py
@@ -12,7 +12,6 @@ import os
 import json
 from algoliasearch.search_client import SearchClient
 
-
 load_dotenv()
 
 app = Flask(__name__)
@@ -44,6 +43,7 @@ class User(UserMixin, db.Model):
     branch = db.Column(db.String(100))
     review = db.relationship("Review", back_populates="author")
 
+
 class Review(db.Model):
     __tablename__ = 'review'
     id = db.Column(db.Integer, primary_key=True)
@@ -55,7 +55,8 @@ class Review(db.Model):
     attendance = db.Column(db.VARCHAR(250))
     marks = db.Column(db.VARCHAR(250))
     research = db.Column(db.VARCHAR(250))
-    text = db.Column(db.TEXT, nullable=False)
+    # text = db.Column(db.TEXT, nullable=False)
+
 
 class ProfData(db.Model):
     __tablename__ = 'prof_data'
@@ -68,8 +69,6 @@ class ProfData(db.Model):
 
 
 with app.app_context():
-    # x = ProfData.query.filter_by(name="Kaustab Ghosh").first()
-    # print(x)
     # for elem in data:
     #     new_prof = ProfData(name=elem["name"],designation=elem["designation"],link=elem["link"],image=elem["image"])
     #     db.session.add(new_prof)
@@ -79,23 +78,33 @@ with app.app_context():
     rev = Review.query.all()
     c = User.query.all()
 
+
 @login_manager.unauthorized_handler
 def unauthorized():
     return redirect(url_for('login'))
+
+
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
+
+
 @app.route("/")
 def home():
     return render_template("index.html")
+
+
 @app.route("/all-prof/<num>")
 def all_prof(num):
     prof_ratings = {}
     for prof in data:
-        avg_rating_da = db.session.query(func.avg(Review.da)).filter_by(prof_id=prof.id).scalar()
-        avg_rating_attend = db.session.query(func.avg(Review.attendance)).filter_by(prof_id=prof.id).scalar()
-        avg_rating_marks = db.session.query(func.avg(Review.marks)).filter_by(prof_id=prof.id).scalar()
-        avg_rating_research = db.session.query(func.avg(Review.research)).filter_by(prof_id=prof.id).scalar()
+        avg_rating_da = db.session.query(func.avg(Review.da)).filter_by(prof_id=prof.id).filter(Review.da != 0).scalar()
+        avg_rating_attend = db.session.query(func.avg(Review.attendance)).filter_by(prof_id=prof.id).filter(
+            Review.attendance != 0).scalar()
+        avg_rating_marks = db.session.query(func.avg(Review.marks)).filter_by(prof_id=prof.id).filter(
+            Review.marks != 0).scalar()
+        avg_rating_research = db.session.query(func.avg(Review.research)).filter_by(prof_id=prof.id).filter(
+            Review.research != 0).scalar()
 
         try:
             avg_rating_da = round(avg_rating_da, 1)
@@ -104,12 +113,15 @@ def all_prof(num):
             avg_rating_research = round(avg_rating_research, 1)
             # avg_rating_da = db.session.query(func.avg(Review.da)).filter_by(prof_id=prof.id).scalar()
             # avg_rating_da = round(avg_rating_da, 1)
-            prof_ratings[prof.id] = {"da":avg_rating_da,"attendance":avg_rating_attend,"marks":avg_rating_marks, "re":avg_rating_research}
+            prof_ratings[prof.id] = {"da": avg_rating_da, "attendance": avg_rating_attend, "marks": avg_rating_marks,
+                                     "re": avg_rating_research}
         except TypeError:
             pass
 
-    return render_template("no-sidebar.html",l=data,n=num,r=rev,rating = prof_ratings)
-@app.route("/signup", methods=["GET","POST"])
+    return render_template("no-sidebar.html", l=data, n=num, r=rev, rating=prof_ratings)
+
+
+@app.route("/signup", methods=["GET", "POST"])
 def signup():
     if request.method == "POST":
         name = request.form.get("name")
@@ -117,18 +129,20 @@ def signup():
         branch = request.form.get("branch")
         year = request.form.get("year")
         if name == "" or email == "" or branch == "" or year == "" or request.form.get("password") == "":
-            return render_template('signup.html',l=1,p=0)
-        if len(request.form.get("password"))<8:
-            return render_template('signup.html',l=0,p=1)
-        password = (generate_password_hash(request.form.get("password"), method='pbkdf2:sha256:260000', salt_length=100))[21:]
-        new_user = User(name=name,email=email,branch=branch,year=year,password=password)
+            return render_template('signup.html', l=1, p=0)
+        if len(request.form.get("password")) < 8:
+            return render_template('signup.html', l=0, p=1)
+        password = (generate_password_hash(request.form.get("password"), method='pbkdf2:sha256:260000',
+                                           salt_length=100))[21:]
+        new_user = User(name=name, email=email, branch=branch, year=year, password=password)
         db.session.add(new_user)
         db.session.commit()
         login_user(new_user)
         return redirect("/")
-    return render_template("signup.html",l=0,p=0)
+    return render_template("signup.html", l=0, p=0)
 
-@app.route("/login", methods=["GET","POST"])
+
+@app.route("/login", methods=["GET", "POST"])
 def login():
     if request.method == "POST":
         email = request.form.get("email")
@@ -145,20 +159,25 @@ def login():
             return render_template("login.html", l=1)
     return render_template("login.html", l=0)
 
+
 @app.route('/logout')
 @login_required
 def logout():
     logout_user()
     return redirect("/")
 
-@app.route("/search", methods=["GET","POST"])
+
+@app.route("/search", methods=["GET", "POST"])
 def search():
     prof_ratings = {}
     for prof in data:
-        avg_rating_da = db.session.query(func.avg(Review.da)).filter_by(prof_id=prof.id).scalar()
-        avg_rating_attend = db.session.query(func.avg(Review.attendance)).filter_by(prof_id=prof.id).scalar()
-        avg_rating_marks = db.session.query(func.avg(Review.marks)).filter_by(prof_id=prof.id).scalar()
-        avg_rating_research = db.session.query(func.avg(Review.research)).filter_by(prof_id=prof.id).scalar()
+        avg_rating_da = db.session.query(func.avg(Review.da)).filter_by(prof_id=prof.id).filter(Review.da != 0).scalar()
+        avg_rating_attend = db.session.query(func.avg(Review.attendance)).filter_by(prof_id=prof.id).filter(
+            Review.attendance != 0).scalar()
+        avg_rating_marks = db.session.query(func.avg(Review.marks)).filter_by(prof_id=prof.id).filter(
+            Review.marks != 0).scalar()
+        avg_rating_research = db.session.query(func.avg(Review.research)).filter_by(prof_id=prof.id).filter(
+            Review.research != 0).scalar()
         try:
             avg_rating_da = round(avg_rating_da, 1)
             avg_rating_attend = round(avg_rating_attend, 1)
@@ -166,27 +185,31 @@ def search():
             avg_rating_research = round(avg_rating_research, 1)
             # avg_rating_da = db.session.query(func.avg(Review.da)).filter_by(prof_id=prof.id).scalar()
             # avg_rating_da = round(avg_rating_da, 1)
-            prof_ratings[prof.id] = {"da": avg_rating_da, "attendance": avg_rating_attend, "marks": avg_rating_marks, "reasearch": avg_rating_research}
+            prof_ratings[prof.id] = {"da": avg_rating_da, "attendance": avg_rating_attend, "marks": avg_rating_marks,
+                                     "reasearch": avg_rating_research}
         except TypeError:
             pass
 
     c = request.form.get("searchquery")
     l = (index.search(c))["hits"]
-    return render_template("search.html", li=l,leng=len(l), name=c, rating = prof_ratings)
+    return render_template("search.html", li=l, leng=len(l), name=c, rating=prof_ratings)
 
-@app.route("/review/<num>", methods=["GET","POST"])
+
+@app.route("/review/<num>", methods=["GET", "POST"])
 @login_required
 def review(num):
     if request.method == "POST":
         o = ProfData.query.filter_by(id=int(num)).first()
-        new_review = Review(author=current_user,attendance=request.form.get("attend"),da=request.form.get("da"),marks=request.form.get("marks"),research=request.form.get("research"),text=request.form.get('message'),prof=o)
+        new_review = Review(author=current_user, attendance=request.form.get("attend"), da=request.form.get("da"),
+                            marks=request.form.get("marks"), research=request.form.get("research"), prof=o)
         db.session.add(new_review)
         db.session.commit()
         return redirect("/")
-    return render_template("review.html",n=num)
+    return render_template("review.html", n=num)
+
+
 # @app.route("/comment/<num>", methods=["GET","POST"])
 # def comment(num):
-
 
 
 if __name__ == "__main__":
